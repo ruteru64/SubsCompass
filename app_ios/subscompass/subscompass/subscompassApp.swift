7268a508eab8c495877763a52c5ee88928733b45
ruteru64 <ruteru64@gmail.com>
2022-06-25 01:26:45 +0900

    create:ios app prjec

---
+++ app_ios/subscompass/subscompass/subscompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  subscompassApp.swift
+//  subscompass
+//
+//  Created by teruki on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct subscompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
